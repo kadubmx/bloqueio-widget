d0c9f3b3cc630616bb1a30c88469869827949e05
Carlos Dias <kadubmx@gmail.com>
2025-07-22 04:08:22 -0300

    feat: BloqueioWidget export iife + external React

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    {
+    jsxRuntime: 'classic' 
+  }
+  )],
   define: { 'process.env.NODE_ENV': JSON.stringify('production') },
+  
 
   build: {
     lib: {
